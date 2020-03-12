ff83c01cbb59feaa264833c7af9198b67aab184a
ak7907 <ak7907@nyu.edu>
2020-03-12 00:43:52 -0400

    eslint-disable no-undef 02

--- corelink.js
+++ corelink.js
@@ -287,7 +287,7 @@ function listStreams() {
 
   for (var s in source) {
     for (token in tokens) {
-      for (let key in tokens[token]['streams']) {
+      for (var key in tokens[token]['streams']) {
         if (tokens[token]['streams'][key] == s) {
           var user = users[tokens[token]['user']]['username']
           break
