02694ff2e07068936b8d6925c8e579211cfd1728
ak7907 <ak7907@nyu.edu>
2020-03-12 10:48:45 -0400

    eslint-disable dot-notation

--- corelink.js
+++ corelink.js
@@ -6,7 +6,6 @@
 /* eslint-disable func-names */
 /* eslint-disable block-scoped-var */
 /* eslint-disable no-undef */    // 965:25 error 'app' is not defined no-undef 966:51 error 'app' is not defined no-undef 2371:40 error 'streamid' is not defined no-undef 
-/* eslint-disable dot-notation */
 /* eslint-disable eqeqeq */
 /* eslint-disable no-var */
 /* eslint-disable prefer-template */
