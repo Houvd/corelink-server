65b48abbf17045fa6e0aa02ae97cdcd03afaa71a
Robert Pahle <robert.pahle@gmail.com>
2020-03-15 02:48:01 -0400

    initial groups and rooms migration

---
+++ knex/migrations/20200315024218_create_table_rooms.js
@@ -0,0 +1,10 @@
+exports.up = (knex) => knex.schema.createTable('rooms', (table) => {
+  table.increments()
+  table.integer('owner_id').notNullable().unsigned()
+  table.foreign('owner_id').references('id').inTable('users')
+  table.string('room_name').notNullable()
+  table.timestamp('created_at').default(knex.fn.now())
+  table.timestamp('updated_at').default(knex.fn.now())
+})
+
+exports.down = (knex) => knex.schema.dropTable('rooms')
