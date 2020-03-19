7c590dd59d94f45da4105eaeceb86bd6c7faa42d
ak7907 <ak7907@nyu.edu>
2020-03-19 12:49:06 -0400

    eslint for -if guard 03

--- corelink.js
+++ corelink.js
@@ -1,4 +1,4 @@
-/* eslint-disable guard-for-in */
+// /* eslint-disable guard-for-in */
 
 /* eslint-disable no-restricted-syntax */
 
@@ -9,8 +9,6 @@
 /* eslint-disable no-shadow */
 /* eslint-disable no-param-reassign */
 
-// I was not able to push, so i added these again:
-// /* eslint-disable guard-for-in */
 
 /**
  * @file NodeJS Corelink core server
@@ -1554,11 +1552,13 @@ functions.receiver = new Object({
 
         // designate streams to be directly relayed ot this target
         for (stream in message.streamlist) {
+          if (stream) {
           // send subscriber message to sender streams that are newly subscribed to
-          if (typeof streamrelay[message.streamlist[stream].streamid][streamid] === 'undefined') {
-            serverfunctions.subscriber.process(message.streamlist[stream].streamid, streamid)
+            if (typeof streamrelay[message.streamlist[stream].streamid][streamid] === 'undefined') {
+              serverfunctions.subscriber.process(message.streamlist[stream].streamid, streamid)
+            }
+            streamrelay[message.streamlist[stream].streamid][streamid] = []
           }
-          streamrelay[message.streamlist[stream].streamid][streamid] = []
         }
 
         // create result for client to connect as a receiver
@@ -1655,9 +1655,11 @@ functions.subscribe = new Object({
 
         // add the already subscribed streams
         for (s in streamrelay) {
-          for (t in streamrelay[s]) {
-            if ((t === message.receiverid) && (!message.streamid.includes(s))) {
-              message.streamid.push(s)
+          if (s) {
+            for (t in streamrelay[s]) {
+              if ((t === message.receiverid) && (!message.streamid.includes(s))) {
+                message.streamid.push(s)
+              }
             }
           }
         }
@@ -1674,25 +1676,29 @@ functions.subscribe = new Object({
         // add usernames to the specific streams
         message.streamlist = []
         for (stream in message.streamid) {
-          streamlistelement = {}
-          streamlistelement.streamid = message.streamid[stream]
-          streamlistelement.type = source[message.streamid[stream]].type
-          streamlistelement.meta = source[message.streamid[stream]].meta
+          if (stream) {
+            streamlistelement = {}
+            streamlistelement.streamid = message.streamid[stream]
+            streamlistelement.type = source[message.streamid[stream]].type
+            streamlistelement.meta = source[message.streamid[stream]].meta
 
-          // add apps processing list for streams that are processed, otherwise leave empty
-          // walk through source from tags until we find user, add apps and user
-          userApps = findApps(message.streamid[stream])
-          streamlistelement.user = userApps.user
-          streamlistelement.apps = userApps.apps
+            // add apps processing list for streams that are processed, otherwise leave empty
+            // walk through source from tags until we find user, add apps and user
+            userApps = findApps(message.streamid[stream])
+            streamlistelement.user = userApps.user
+            streamlistelement.apps = userApps.apps
 
-          message.streamlist.push(streamlistelement)
+            message.streamlist.push(streamlistelement)
+          }
         }
 
         // designate streams to be directly relayed ot this target
         for (stream in message.streamlist) {
           // send subscriber message to sender streams that are newly subscribed to
-          if (typeof streamrelay[message.streamlist[stream].streamid][message.receiverid] === 'undefined') serverfunctions.subscriber.process(message.streamlist[stream].streamid, message.receiverid)
-          streamrelay[message.streamlist[stream].streamid][message.receiverid] = []
+          if (stream) {
+            if (typeof streamrelay[message.streamlist[stream].streamid][message.receiverid] === 'undefined') serverfunctions.subscriber.process(message.streamlist[stream].streamid, message.receiverid)
+            streamrelay[message.streamlist[stream].streamid][message.receiverid] = []
+          }
         }
 
         // create result for client to connect as a receiver
@@ -1767,12 +1773,16 @@ functions.unsubscribe = new Object({
                 && (('streamid' in message) && (message.streamid.length > 0))) {
         // unsubscribe streams
         for (s in streamrelay) {
-          for (t in streamrelay[s]) {
-            if ((t === message.receiverid) && (message.streamid.includes(s))) {
-              delete streamrelay[s][t]
-              // send dropped message to sender streams that are newly subscribed to
-              serverfunctions.dropped.process(s, t)
-              if (streamrelay[s].length === 0) delete streamrelay[s]
+          if (s) {
+            for (t in streamrelay[s]) {
+              if (t) {
+                if ((t === message.receiverid) && (message.streamid.includes(s))) {
+                  delete streamrelay[s][t]
+                  // send dropped message to sender streams that are newly subscribed to
+                  serverfunctions.dropped.process(s, t)
+                  if (streamrelay[s].length === 0) delete streamrelay[s]
+                }
+              }
             }
           }
         }
@@ -1780,26 +1790,32 @@ functions.unsubscribe = new Object({
         // create list of subscribed streams
         message.streamid = []
         for (s in streamrelay) {
-          for (t in streamrelay[s]) {
-            if (t === message.receiverid) message.streamid.push(s)
+          if (s) {
+            for (t in streamrelay[s]) {
+              if (t) {
+                if (t === message.receiverid) message.streamid.push(s)
+              }
+            }
           }
         }
 
         // add usernames to the specific streams
         message.streamlist = []
         for (stream in message.streamid) {
-          streamlistelement = {}
-          streamlistelement.streamid = message.streamid[stream]
-          streamlistelement.type = source[message.streamid[stream]].type
-          streamlistelement.meta = source[message.streamid[stream]].meta
+          if (stream) {
+            streamlistelement = {}
+            streamlistelement.streamid = message.streamid[stream]
+            streamlistelement.type = source[message.streamid[stream]].type
+            streamlistelement.meta = source[message.streamid[stream]].meta
 
-          // add apps processing list for streams that are processed, otherwise leave empty
-          // walk through source from tags until we find user, add apps and user
-          userApps = findApps(message.streamid[stream])
-          streamlistelement.user = userApps.user
-          streamlistelement.apps = userApps.apps
+            // add apps processing list for streams that are processed, otherwise leave empty
+            // walk through source from tags until we find user, add apps and user
+            userApps = findApps(message.streamid[stream])
+            streamlistelement.user = userApps.user
+            streamlistelement.apps = userApps.apps
 
-          message.streamlist.push(streamlistelement)
+            message.streamlist.push(streamlistelement)
+          }
         }
 
         // create result for client to connect as a receiver
