3d2db3deb6ef8ff5e3e0a920e519313e4ac03785
Robert Pahle <robert.pahle@gmail.com>
2020-03-24 23:24:45 -0400

    removed table types, use them as tags instead

--- knex/migrations/20200316030047_create_table_types.js
+++
@@ -1,8 +0,0 @@
-exports.up = (knex) => knex.schema.createTable('types', (table) => {
-  table.increments()
-  table.string('type_name').notNullable()
-  table.timestamp('created_at').default(knex.fn.now())
-  table.timestamp('updated_at').default(knex.fn.now())
-})
-
-exports.down = (knex) => knex.schema.dropTable('types')
