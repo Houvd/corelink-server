753f2492696cc0b4d18b56d0df8fac0ea1d1dac8
Robert Pahle <robert.pahle@gmail.com>
2020-04-23 09:53:35 -0400

    fix properties issue on server

--- corelink.js
+++ corelink.js
@@ -3748,7 +3748,7 @@ async function run() {
         console.log(`Received message not a proper JSON:${data.toString()}`)
         return
       }
-      if ('function' in message) {
+      if (('function' in message) && (message.function in functions)) {
         if (message.function === 'auth') send = JSON.stringify(await functions[message.function].process(message, remoteAddress, conn))
         else send = JSON.stringify(await functions[message.function].process(message))
         if ('id' in message) {
