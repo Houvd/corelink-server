5918fa6d03184646ca5203034ccfd20002ed0d47
Abhishek Khanna <ak7907@nyu.edu>
2020-09-22 19:45:24 -0400

    unsubcribe function Fix

--- corelink.js
+++ corelink.js
@@ -2985,7 +2985,7 @@ async function run() {
         },
         receiverID: {
           description: 'set the existing receiver streamID',
-          type: 'string',
+          type: 'number',
           sample: '$$receiver.streamID',
         },
         streamID: {
@@ -3026,8 +3026,9 @@ async function run() {
       const workMessage = message
       if (typeof data !== 'object') {
         console.log('*** unsubscribe ***')
+        if ('receiverID' in workMessage) { console.log('checkk1') }
         if ((('receiverID' in workMessage) && (typeof workMessage.receiverID === 'number') && (typeof target[workMessage.receiverID] !== 'undefined'))
-                  && (('streamID' in workMessage) && (typeof workMessage.streamID === 'number'))) {
+                  && (('streamID' in workMessage) && (Array.isArray(workMessage.streamID)))) {
           // unsubscribe streams
           workMessage.streamList = []
           for (s in streamRelay) {
