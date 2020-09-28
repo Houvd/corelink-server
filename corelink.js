65b95bb6ea3dd309d5cff6149550b583d5dd8d26
Robert Pahle <pahle@nyu.edu>
2020-09-28 18:45:41 -0400

    fixed type test in unsubscribe

--- corelink.js
+++ corelink.js
@@ -2985,7 +2985,7 @@ async function run() {
         },
         receiverID: {
           description: 'set the existing receiver streamID',
-          type: 'number',
+          type: 'string',
           sample: '$$receiver.streamID',
         },
         streamID: {
@@ -3026,9 +3026,8 @@ async function run() {
       const workMessage = message
       if (typeof data !== 'object') {
         console.log('*** unsubscribe ***')
-        if ('receiverID' in workMessage) { console.log('checkk1') }
         if ((('receiverID' in workMessage) && (typeof workMessage.receiverID === 'number') && (typeof target[workMessage.receiverID] !== 'undefined'))
-                  && (('streamID' in workMessage) && (Array.isArray(workMessage.streamID)))) {
+                  && (('streamID' in workMessage) && ((typeof workMessage.streamID === 'number') || (Array.isArray(workMessage.streamID))))) {
           // unsubscribe streams
           workMessage.streamList = []
           for (s in streamRelay) {
@@ -3924,6 +3923,7 @@ async function run() {
   UDPDataServer.bind(port.udp)
 
   function relayData(msg, remoteAddress, remotePort) {
+
     // decode header
     let headerSize = msg.readUInt16LE(0)
     const dataSize = msg.readUInt16LE(2)
