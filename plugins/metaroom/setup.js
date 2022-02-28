a92ede526a39aa1150cc980fe4134f66b8b5a7de
Robert Pahle <robert.pahle@gmail.com>
2022-02-28 18:46:10 -0500

    remove plugins, they will go to plugin repo

--- plugins/metaroom/setup.js
+++
@@ -1,6 +0,0 @@
-const fs = require('fs');
-
-// destination.txt will be created or overwritten by default.
-fs.copyFile('../../../lib/corelink.lib.js', 'corelink.lib.js', (err) => {
-  if (err) throw err;
-});
